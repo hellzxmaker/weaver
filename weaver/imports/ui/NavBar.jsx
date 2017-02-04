38903e8392d39b7ca165e94f841224e9f1e0ba65
Michael Bazik <hellzxmaker@gmail.com>
2017-02-04 14:46:59 -0500

    Added menu to appbar.

--- weaver/imports/ui/NavBar.jsx
+++ weaver/imports/ui/NavBar.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 import AppBar from 'material-ui/AppBar';
-import AddBox from 'material-ui/svg-icons/content/add-box';
+import AddBox from 'material-ui/svg-icons/content/add-box'
+import Menu from 'material-ui/svg-icons/navigation/menu';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 
 import CreateExperienceDialog from './CreateExperienceDialog.jsx';
@@ -13,6 +16,7 @@ export default class NavBar extends Component {
 
     this.state = {
       showNewExperienceDialog: false,
+      showMenuDialog: false,
     };
   }
 
@@ -22,12 +26,39 @@ export default class NavBar extends Component {
     });
   }
 
+  toggleMenuDialog() {
+    this.setState({
+      showMenuDialog: !this.state.showMenuDialog,
+    });
+  }
+
   renderNewExperienceDialog() {
     if ( this.state.showNewExperienceDialog ) {
       return (
       <div>
         <div id="app-bar">
-          <AppBar iconElementRight={ <IconButton label="New Experience" onTouchTap={ this.toggleNewExperienceDialog.bind(this) }><AddBox/></IconButton> } />
+          <AppBar iconElementRight={
+                    <IconButton label="New Experience"
+                      onTouchTap={ this.toggleNewExperienceDialog.bind(this) }
+                    >
+                    <AddBox/>
+                    </IconButton>
+                  }
+                  iconElementLeft={
+                    <IconMenu
+                      iconButtonElement={<IconButton><Menu/></IconButton>}
+                      anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                      targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                      onTouchTap={ this.toggleMenuDialog.bind(this) }
+                    >
+                      <MenuItem primaryText="Refresh" />
+                      <MenuItem primaryText="Send feedback" />
+                      <MenuItem primaryText="Settings" />
+                      <MenuItem primaryText="Help" />
+                      <MenuItem primaryText="Sign out" />
+                    </IconMenu>
+                  }
+          />
         </div>
         <div id="create-new-experience-dialog">
           <CreateExperienceDialog open={ this.state.showNewExperienceDialog } />
@@ -37,7 +68,28 @@ export default class NavBar extends Component {
     } else {
         return (
           <div id="app-bar">
-            <AppBar iconElementRight={ <IconButton label="New Experience" tooltip="Add Experience" onTouchTap={ this.toggleNewExperienceDialog.bind(this) }><AddBox/></IconButton> } />
+            <AppBar iconElementRight={
+                      <IconButton label="New Experience"
+                        onTouchTap={ this.toggleNewExperienceDialog.bind(this) }
+                      >
+                      <AddBox/>
+                      </IconButton>
+                    }
+                    iconElementLeft={
+                      <IconMenu
+                        iconButtonElement={<IconButton><Menu color="000000" /></IconButton>}
+                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                        targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                        onTouchTap={ this.toggleMenuDialog.bind(this) }
+                      >
+                        <MenuItem primaryText="Refresh" />
+                        <MenuItem primaryText="Send feedback" />
+                        <MenuItem primaryText="Settings" />
+                        <MenuItem primaryText="Help" />
+                        <MenuItem primaryText="Sign out" />
+                      </IconMenu>
+                    }
+              />
           </div>
         );
     }
